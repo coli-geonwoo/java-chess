9790f2545b7b22ca396b4d1deee0030820fc5c8e
coli-geonwoo <kkwoo001021@naver.com>
2024-03-27 15:35:40 +0900

    feat: Score 연산 메서드 및 eq&hc 추가

--- src/main/java/chess/domain/board/Score.java
+++ src/main/java/chess/domain/board/Score.java
@@ -1,5 +1,7 @@
 package chess.domain.board;
 
+import java.util.Objects;
+
 public final class Score {
     private final double score;
 
@@ -12,10 +14,31 @@ public final class Score {
     }
 
     public Score add(Score otherScore) {
-        return Score.from(score + otherScore.getScore());
+        return Score.from(this.score + otherScore.getScore());
+    }
+
+    public Score minus(Score otherScore) {
+        return Score.from(this.score - otherScore.getScore());
+    }
+
+    public Score multiply(Long multiplier) {
+        return Score.from(this.score * multiplier);
     }
 
     public double getScore() {
         return score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Score score1 = (Score) o;
+        return Double.compare(score, score1.score) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score);
     }
 }
