75b0732d0216cdd1b28fde01894fe8ef5f8a4684
coli-geonwoo <kkwoo001021@naver.com>
2024-04-03 14:19:44 +0900

    test: setUp함수를 통한 중복 코드 분리

--- src/test/java/chess/service/ChessGameServiceTest.java
+++ src/test/java/chess/service/ChessGameServiceTest.java
@@ -1,38 +1,45 @@
 package chess.service;
 
+import chess.domain.board.ChessBoard;
 import chess.domain.game.ChessGame;
-import chess.view.OutputView;
+import chess.domain.piece.NullPiece;
+import chess.domain.piece.Piece;
+import chess.domain.piece.WhitePawn;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static chess.domain.position.Fixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-// TODO outputView를 활용하지 않고 테스트 구성해보기
-// TODO setup 함수를 통해 초기화해보기
 class ChessGameServiceTest {
+    private ChessGame game;
+    private ChessGameService service;
+
+    @BeforeEach
+    void setUp() {
+        game = ChessGame.newGame();
+        service = new ChessGameService(new FakeBoardDao(game.getBoard()), new FakeTurnDao(game.getTurn()), game);
+    }
 
     @DisplayName("기물 이동을 통해 턴을 진행할 수 있다")
     @Test
     void should_PlayTurn_When_GiveStartAndDestination() {
-        OutputView outputView = new OutputView();
-        ChessGame newGame = ChessGame.newGame();
-        ChessGameService service1 = new ChessGameService(new FakeBoardDao(newGame.getBoard()), new FakeTurnDao(newGame.getTurn()), newGame);
-        ChessGame movedGame = ChessGame.newGame();
-        movedGame.playTurn(D2, D4);
+        service.playTurn(D2, D4);
+        ChessBoard gameBoard = service.gameBoard();
+        Piece startPositionPiece = gameBoard.findPieceByPosition(D2);
+        Piece destinationPositionPiece = gameBoard.findPieceByPosition(D4);
 
-        service1.playTurn(D2, D4);
-        String expectedGame = outputView.printChessBoardMessage(movedGame.getBoard());
-        String actualGame = outputView.printChessBoardMessage(service1.gameBoard());
-
-        assertThat(actualGame).isEqualTo(expectedGame);
+        assertAll(
+                () -> assertThat(startPositionPiece.getClass()).isEqualTo(NullPiece.class),
+                () -> assertThat(destinationPositionPiece.getClass()).isEqualTo(WhitePawn.class)
+        );
     }
 
     @DisplayName("게임이 끝났는지 알 수 있다")
     @Test
-    void should_NoticeEndGame(){
-        ChessGame newGame = ChessGame.newGame();
-        ChessGameService service = new ChessGameService(new FakeBoardDao(newGame.getBoard()), new FakeTurnDao(newGame.getTurn()), newGame);
+    void should_NoticeEndGame() {
         service.playTurn(E2, E3);
         service.playTurn(F7, F6);
         service.playTurn(D1, H5);
@@ -44,38 +51,29 @@ class ChessGameServiceTest {
 
     @DisplayName("게임이 끝났다면 새 게임을 저장한다")
     @Test
-    void should_SaveNewGame_When_IsEndGame(){
-        OutputView outputView = new OutputView();
-        ChessGame newGame = ChessGame.newGame();
-        ChessGameService service = new ChessGameService(new FakeBoardDao(newGame.getBoard()), new FakeTurnDao(newGame.getTurn()), newGame);
+    void should_SaveNewGame_When_IsEndGame() {
         service.playTurn(E2, E3);
         service.playTurn(F7, F6);
         service.playTurn(D1, H5);
         service.playTurn(G8, H6);
-        service.playTurn(H5, E8);
+        service.playTurn(H5, E8); // 경기 종료
         service.end();
 
-        ChessGameService service2 = ChessGameService.of(new FakeBoardDao(newGame.getBoard()), new FakeTurnDao(newGame.getTurn()));
-        String actualGame = outputView.printChessBoardMessage(service2.gameBoard());
-        String expectedGame = outputView.printChessBoardMessage(ChessGame.newGame().getBoard());
+        service = ChessGameService.of(new FakeBoardDao(game.getBoard()), new FakeTurnDao(game.getTurn()));
 
-        assertThat(actualGame).isEqualTo(expectedGame);
+        assertThat(service.isEndGame()).isFalse();
     }
 
     @DisplayName("게임이 끝나지 않았다면 기존 게임을 저장한다")
     @Test
-    void should_SavePreviousGame_When_IsNotEndGame(){
-        OutputView outputView = new OutputView();
-        ChessGame newGame = ChessGame.newGame();
-        ChessGameService service = new ChessGameService(new FakeBoardDao(newGame.getBoard()), new FakeTurnDao(newGame.getTurn()), newGame);
+    void should_SavePreviousGame_When_IsNotEndGame() {
         service.playTurn(E2, E3);
         service.playTurn(F7, F6);
 
         service.end();
-        service = ChessGameService.of(new FakeBoardDao(newGame.getBoard()), new FakeTurnDao(newGame.getTurn()));
-        String actualGame = outputView.printChessBoardMessage(service.gameBoard());
-        String expectedGame = outputView.printChessBoardMessage(newGame.getBoard());
+        service = ChessGameService.of(new FakeBoardDao(game.getBoard()), new FakeTurnDao(game.getTurn()));
+        ChessBoard actualBoard = service.getGame().getBoard();
 
-        assertThat(actualGame).isEqualTo(expectedGame);
+        assertThat(actualBoard).isEqualTo(game.getBoard());
     }
 }
